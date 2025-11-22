e6b5dd8ba612cf4f22dd4f17b4ee4d146d5e07e4
vincenzo098 <vincenzo.spatuzza098@gmail.com>
2025-11-22 19:17:53 -0300

    Created a script automatically build and deploy my application to github in the package.json. Also developed a simple mongoDB database, still not connected to the application, but the mongo.js file allows for users to add a new register or print all registers saved in the database depending on the arguments passed when running the mongo.js file

---
+++ backend/mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3){
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const [,, password, name, number] = process.argv
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.arcrdkv.mongodb.net/?appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, { family:4 })
+
+const personSchema = new mongoose.Schema({
+    id: Number,
+    name: String, 
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (name && number){
+    //passing the arguments as variables to person
+    const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+    })
+
+    //Inputing register to MongoDB
+    person.save().then(result => {
+    console.log('register saved!')
+    mongoose.connection.close()
+    })
+
+} else {
+    
+    console.log("phonebook:")
+
+    Person.find({}).then(result => {
+    result.forEach(person => {
+        console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+    })
+}
